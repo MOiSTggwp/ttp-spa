fdeedaf38e5f64061e21a4d96d262d70b9d927a1
MOiSTggwp <123Mri321Ma0Ru>
2022-01-12 02:46:43 +0300

    Start project

---
+++ resources/js/components/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <router-link to="{ name: 'Home'}">Home</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
